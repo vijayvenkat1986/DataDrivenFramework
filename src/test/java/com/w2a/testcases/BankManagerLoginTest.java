7b023a4510020fbcd14ec17680ec08aeff5335e6
vijayvenkat1986 <63242421+vijayvenkat1986@users.noreply.github.com>
2020-04-27 19:45:34 +0530

    Update BankManagerLoginTest.java

--- src/test/java/com/w2a/testcases/BankManagerLoginTest.java
+++ src/test/java/com/w2a/testcases/BankManagerLoginTest.java
@@ -5,6 +5,9 @@ import org.testng.AssertJUnit;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
+
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,17 +17,15 @@ import com.w2a.base.TestBase;
 public class BankManagerLoginTest extends TestBase {
 	
 	@Test
-	public void loginAsBankManager() throws InterruptedException{
-		
-		
+	public void bankManagerLoginTest() throws InterruptedException, IOException{
 		
 		log.debug("Inside Login Test ");
-		driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();
+		//driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();
+		click("bmlBtn_CSS");
 		
-		Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("addCustBtn"))),"Login Not Successful");
+		Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("addCustBtn_CSS"))),"Login Not Successful");
 		
-		Reporter.log("Login Successfully executed");
-		
+		//Reporter.log("Login Successfully executed");
 		
 	}
 
