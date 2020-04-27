d24eb23cfc157880a089006d04b85c6c5a36ea62
vijayvenkat1986 <63242421+vijayvenkat1986@users.noreply.github.com>
2020-04-27 19:43:16 +0530

    Update TestBase.java

--- src/test/java/com/w2a/base/TestBase.java
+++ src/test/java/com/w2a/base/TestBase.java
@@ -9,14 +9,23 @@ import java.util.concurrent.TimeUnit;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import com.w2a.utilities.ExcelReader;
+import com.w2a.utilities.ExtentManager;
+import com.w2a.utilities.TestUtil;
 
 public class TestBase 
 {
@@ -40,6 +49,8 @@ public class TestBase
 	public static Logger log = Logger.getLogger("devpinoyLogger");
 	public static ExcelReader excel = new ExcelReader(System.getProperty("user.dir")+"\\src\\test\\resources\\excel\\testdata.xlsx");
 	public static WebDriverWait wait;
+	public ExtentReports rep = ExtentManager.getInstance();
+	public static ExtentTest test;
 		
 	
 	@BeforeSuite
@@ -95,6 +106,71 @@ public class TestBase
 	  	  driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")),TimeUnit.SECONDS);
 	  	  wait = new WebDriverWait(driver,5);
 	  	}
+	}
+	
+	public void click(String locator)
+	{
+		if (locator.endsWith("_CSS")) {
+			driver.findElement(By.cssSelector(OR.getProperty(locator))).click();
+		} else if (locator.endsWith("_XPATH")) {
+			driver.findElement(By.xpath(OR.getProperty(locator))).click();
+		} else if (locator.endsWith("_ID")) {
+			driver.findElement(By.id(OR.getProperty(locator))).click();
+		}
+		test.log(LogStatus.INFO, "Clicking on :" +locator);
+	}
+	
+	static WebElement dropdown;
+	
+	public void select(String locator, String value) {
+
+		if (locator.endsWith("_CSS")) {
+			dropdown = driver.findElement(By.cssSelector(OR.getProperty(locator)));
+		} else if (locator.endsWith("_XPATH")) {
+			dropdown = driver.findElement(By.xpath(OR.getProperty(locator)));
+		} else if (locator.endsWith("_ID")) {
+			dropdown = driver.findElement(By.id(OR.getProperty(locator)));
+		}
+		
+		Select select = new Select(dropdown);
+		select.selectByVisibleText(value);
+
+		test.log(LogStatus.INFO, "Selecting from dropdown : " + locator + " value as " + value);
+
+	}
+	
+	public static void verifyEquals(String expected, String actual) throws IOException
+	{
+		try{
+			
+			Assert.assertEquals(actual, expected);
+			
+		}catch(Throwable t)
+		{
+			TestUtil.captureScreenshot();
+			
+			//ReportNG
+			Reporter.log("<br>"+"Verification Failure: "+t.getMessage()+"<br");
+			Reporter.log("<a target =\"_blank\" href="+TestUtil.screenshotName+"><img src="+TestUtil.screenshotName+" height=200 width=200></img></a>");
+			Reporter.log("<br>");
+			Reporter.log("<br>");
+
+			//Extent Reports
+			test.log(LogStatus.FAIL, "Verification failed with exception: " +t.getMessage());
+			test.log(LogStatus.FAIL, test.addScreenCapture(TestUtil.screenshotName));
+		}
+	}
+	
+	public void type(String locator, String value)
+	{
+		if (locator.endsWith("_CSS")) {
+			driver.findElement(By.cssSelector(OR.getProperty(locator))).sendKeys(value);
+		} else if (locator.endsWith("_XPATH")) {
+			driver.findElement(By.xpath(OR.getProperty(locator))).sendKeys(value);
+		} else if (locator.endsWith("_ID")) {
+			driver.findElement(By.id(OR.getProperty(locator))).sendKeys(value);
+		}
+		test.log(LogStatus.INFO, "Typing in: " +locator+" entered value as "+value);
 	}
 	
 	public boolean isElementPresent(By by)
