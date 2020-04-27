407cc8f07c32676659a3e144ce143c732809a55f
vijayvenkat1986 <63242421+vijayvenkat1986@users.noreply.github.com>
2020-04-27 19:43:50 +0530

    Update CustomListeners.java

--- src/test/java/com/w2a/listeners/CustomListeners.java
+++ src/test/java/com/w2a/listeners/CustomListeners.java
@@ -1,11 +1,19 @@
 package com.w2a.listeners;
 
+import java.io.IOException;
+
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
+import org.testng.SkipException;
 
-public class CustomListeners implements ITestListener {
+import com.mysql.cj.core.util.TestUtils;
+import com.relevantcodes.extentreports.LogStatus;
+import com.w2a.base.TestBase;
+import com.w2a.utilities.TestUtil;
+
+public class CustomListeners extends TestBase implements ITestListener {
 
 	public void onFinish(ITestContext arg0) {
 		// TODO Auto-generated method stub
@@ -13,7 +21,7 @@ public class CustomListeners implements ITestListener {
 	}
 
 	public void onStart(ITestContext arg0) {
-		// TODO Auto-generated method stub
+		test = rep.startTest(arg0.getName().toUpperCase());
 		
 	}
 
@@ -25,26 +33,46 @@ public class CustomListeners implements ITestListener {
 	public void onTestFailure(ITestResult arg0) {
 		// TODO Auto-generated method stub
 		System.setProperty("org.uncommons.reportng.escape-output", "false");
-		Reporter.log("Capturing Screenshot");
-		Reporter.log("<a target =\"_blank\" href=\"C:\\Users\\446617\\My Photo.jpg\">Screenshot</a>");
+		try {
+			TestUtil.captureScreenshot();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		test.log(LogStatus.FAIL, arg0.getName().toUpperCase()+" Failed with exception: " +arg0.getThrowable());
+		test.log(LogStatus.FAIL, test.addScreenCapture(TestUtil.screenshotName));
+		
+		Reporter.log("Click to see Screenshot");
+		Reporter.log("<a target =\"_blank\" href="+TestUtil.screenshotName+">Screenshot</a>");
 		Reporter.log("<br>");
-		Reporter.log("<a target =\"_blank\" href=\"C:\\Users\\446617\\My Photo.jpg\"><img src=\"C:\\Users\\446617\\My Photo.jpg\" height=200 width=200></img></a>");
+		Reporter.log("<br>");
+		Reporter.log("<a target =\"_blank\" href="+TestUtil.screenshotName+"><img src="+TestUtil.screenshotName+" height=200 width=200></img></a>");
+		rep.endTest(test);
+		rep.flush();
+		
 		
 	}
 
-	public void onTestSkipped(ITestResult arg0) {
+	public void onTestSkipped(ITestResult arg0) 
+	{
 		// TODO Auto-generated method stub
-		
+		test.log(LogStatus.SKIP, arg0.getName().toUpperCase()+" Skipped the test as the RunMode is set to N");
+		rep.endTest(test);
+		rep.flush();	
 	}
 
-	public void onTestStart(ITestResult arg0) {
+	public void onTestStart(ITestResult arg0) 
+	{
 		// TODO Auto-generated method stub
-		
+		test = rep.startTest(arg0.getName().toUpperCase());		
 	}
 
-	public void onTestSuccess(ITestResult arg0) {
+	public void onTestSuccess(ITestResult arg0) 
+	{
 		// TODO Auto-generated method stub
-		
+		TestBase.test.log(LogStatus.PASS, arg0.getName().toUpperCase()+" PASS");
+		rep.endTest(test);
+		rep.flush();	
 	}
 
 }
