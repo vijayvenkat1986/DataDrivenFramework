d5b178de1ad2abe779c1371043df4a6919cf03b0
vijayvenkat1986 <63242421+vijayvenkat1986@users.noreply.github.com>
2020-04-27 19:45:04 +0530

    Update AddCustomerTest.java

--- src/test/java/com/w2a/testcases/AddCustomerTest.java
+++ src/test/java/com/w2a/testcases/AddCustomerTest.java
@@ -1,5 +1,6 @@
 package com.w2a.testcases;
 
+import java.util.Hashtable;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
@@ -7,56 +8,39 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.Reporter;
+import org.testng.SkipException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.w2a.base.TestBase;
+import com.w2a.utilities.TestUtil;
 
 public class AddCustomerTest extends TestBase
 {
-	@Test(dataProvider = "getData")
-	public void addCustomer(String firstName, String LastName, String PostCode, String alerttext) throws InterruptedException
+	@Test(dataProviderClass=TestUtil.class,dataProvider="dp")
+	public void addCustomerTest(Hashtable<String, String> data) throws InterruptedException
 	{
+		if(!data.get("Runmode").equals("Y"))
+		{
+			throw new SkipException("Skipping the test case as the Runmode set to N");
+		}
 		
-		driver.findElement(By.cssSelector(OR.getProperty("addCustBtn"))).click();
+		/*driver.findElement(By.cssSelector(OR.getProperty("addCustBtn"))).click();
 		driver.findElement(By.cssSelector(OR.getProperty("firstname"))).sendKeys(firstName);	
 		driver.findElement(By.cssSelector(OR.getProperty("lastname"))).sendKeys(LastName);
 		driver.findElement(By.cssSelector(OR.getProperty("postcode"))).sendKeys(PostCode);
-		driver.findElement(By.cssSelector(OR.getProperty("addBtn"))).click();
-		System.out.println("Button Clicked");
+		driver.findElement(By.cssSelector(OR.getProperty("addBtn"))).click();*/
 		
+		click("addCustBtn_CSS");
+		type("firstname_CSS",data.get("firstName"));
+		type("lastname_XPATH",data.get("LastName"));
+		type("postcode_CSS",data.get("PostCode"));
+		click("addBtn_CSS");
+			
 		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
 		
-		Assert.assertTrue(alert.getText().contains(alerttext));
-		
-		Thread.sleep(3000);
-		alert.accept();		
-		
+		Assert.assertTrue(alert.getText().contains(data.get("alerttext")));
+		alert.accept();	
 		//Reporter.log("Customer successfully added");
-		Assert.fail("Customer not added successfully");
 	}
-	
-	@DataProvider
-	public Object[][] getData()
-	{
-		String sheetName = "AddCustomerTest";
-		int rows = excel.getRowCount(sheetName);
-		int cols = excel.getColumnCount(sheetName);
-
-		Object[][] data = new Object[rows - 1][cols];
-		
-
-		for (int rowNum = 2; rowNum <= rows; rowNum++) { // 2
-			
-			for (int colNum = 0; colNum < cols; colNum++) {
-
-				// data[0][0]
-				data[rowNum - 2][colNum] = excel.getCellData(sheetName, colNum, rowNum);
-			}
-		}
-		return data;
-	}
-	
-	
-
 }
